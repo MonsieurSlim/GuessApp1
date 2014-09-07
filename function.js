0262ab0f3a6ca3f6d61b7fd437f177c374aa6d26
Slim <luccy559slim@gmail.com>
2014-09-07 14:26:02 +0100

    Initializing repo for GH pages

---
+++ function.js
@@ -0,0 +1,35 @@
+// $("submitGuess") = document.getElementById('#submit_guess');
+// $ ("resetGame") = document.getElementById('#reset_game');
+// var alertHolder = document.getElementById('alert_area');
+var inputGuess;
+
+var randomNum = Math.floor(Math.random()*100);
+console.log(randomNum);
+var guesser = function(num) {
+while(true) {
+	 inputGuess = parseInt(inputGuess,10);
+			if (num === randomNum) {
+				$("#alert_area").html("Your Guess Was Just Right!");
+			} 
+			else if (num < randomNum) {
+				$("#alert_area").html("Your Guess Was Lower than the Number!");
+				// $("#alert_area").html("Yikes! Too Low!");
+			} else if (num > randomNum) {
+				$("#alert_area").html("Your Guess Was Higher than the Number!");
+				// $("#alert_area").html("Yikes! Too High!");
+			} else {
+				return;
+			}
+			break ;
+		}
+		
+}
+
+$("#submit_guess").click(function (event) { 
+	console.log("button click");
+	inputGuess = parseInt($("#input_guess").val());
+	guesser(inputGuess);
+}); 
+$("#reset_game").click(function(event) {
+	location.reload();
+});
