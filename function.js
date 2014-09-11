a3b3049906ad60551f1da1c9aa2b5fce67d3d2f1
Slim <luccy559slim@gmail.com>
2014-09-11 17:24:28 +0100

    Update

--- function.js
+++ function.js
@@ -2,34 +2,76 @@
 // $ ("resetGame") = document.getElementById('#reset_game');
 // var alertHolder = document.getElementById('alert_area');
 var inputGuess;
+var prevInput;
+var randomNum;
+var newGame = function () {
+	randomNum = Math.floor(Math.random()*100);
+	console.log(randomNum);
+	$("#input_guess").val("");
+}
+newGame();
 
-var randomNum = Math.floor(Math.random()*100);
-console.log(randomNum);
+var validate = function () {
+	return ((inputGuess === "") || (isNaN(inputGuess)) || (inputGuess > 100) || (inputGuess < 0))? false : true ;
+}
+
 var guesser = function(num) {
-while(true) {
 	 inputGuess = parseInt(inputGuess,10);
 			if (num === randomNum) {
 				$("#alert_area").html("Your Guess Was Just Right!");
 			} 
-			else if (num < randomNum) {
-				$("#alert_area").html("Your Guess Was Lower than the Number!");
-				// $("#alert_area").html("Yikes! Too Low!");
-			} else if (num > randomNum) {
-				$("#alert_area").html("Your Guess Was Higher than the Number!");
-				// $("#alert_area").html("Yikes! Too High!");
-			} else {
-				return;
+			if (num < randomNum) {
+					if(num > prevInput) {
+						console.log("You are getting Warmer.....");
+						$("#alert_area").html("You are getting Warmer.....");
+					}
+					else if (num < prevInput) {
+						console.log("You are getting Colder....");
+						$("#alert_area").html("You are getting Colder....");
+					}
+					else if (num === prevInput) {
+						console.log("Guess same as previous guess....");
+						$("#alert_area").html("Guess same as previous guess....");
+					}
+					else {
+						console.log("Your guess was smaller than the secret Number!");
+						$("#alert_area").html("Your guess was smaller than the secret Number!");
+					}
+								
+			} if (num > randomNum) {
+					if (num < prevInput) {
+						console.log("You are getting Warmer....");
+						$("#alert_area").html("You are getting Warmer....");
+					}
+					else if (num > prevInput) {
+						console.log("You are getting Colder.....");
+						$("#alert_area").html("You are getting Colder.....");
+					}
+					else if (num === prevInput) {
+						console.log("Guess same as previous guess....");
+						$("#alert_area").html("Guess same as previous guess....");
+					}
+					else {
+						console.log("Your guess was greater than the secret Number!");
+						$("#alert_area").html("Your guess was greater than the secret Number!");
+					}
+				
 			}
-			break ;
-		}
 		
 }
 
-$("#submit_guess").click(function (event) { 
+$("#submit_guess").click(function (event) {
+	event.preventDefault(); 
 	console.log("button click");
 	inputGuess = parseInt($("#input_guess").val());
-	guesser(inputGuess);
+	if(validate()) {
+		guesser(inputGuess);
+		prevInput = inputGuess;
+	}else{
+		alert("Please Enter An Integer Between 0 and 100!");
+	}
 }); 
+
 $("#reset_game").click(function(event) {
-	location.reload();
+	newGame();
 });
